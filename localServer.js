2a60e761f3f0c80a80eddbefc17fee05e2e62b30
Mihir611 <mihir17.udupa@gmail.com>
2026-03-26 08:08:14 +0530

    Fixed the issue with the waitlist

--- localServer.js
+++ localServer.js
@@ -41,6 +41,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: [
         'http://localhost:5000',
+        'http://localhost:3000',
+        'http://localhost:5173',
         /\.vercel\.app$/  // Better wildcard support
     ],
     credentials: true,
